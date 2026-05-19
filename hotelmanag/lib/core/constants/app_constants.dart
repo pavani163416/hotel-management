2d660837746c4aaca264643e8a5eb7111d65b411
Abdulrahima2024 <Abdulrahima2024@users.noreply.github.com>
2026-05-19 15:24:49 +0530

    Optimize apiBaseUrl to localhost:5000 and leverage ADB reverse for physical and emulator Android testing

--- hotelmanag/lib/core/constants/app_constants.dart
+++ hotelmanag/lib/core/constants/app_constants.dart
@@ -1,20 +1,6 @@
-import 'package:flutter/foundation.dart';
-import 'dart:io' show Platform;
-
 class AppConstants {
   static const String appName = 'HotelManag';
-  
-  static String get apiBaseUrl {
-    if (kIsWeb) {
-      return 'http://localhost:5000/api/';
-    }
-    try {
-      if (Platform.isAndroid) {
-        return 'http://10.0.2.2:5000/api/';
-      }
-    } catch (_) {}
-    return 'http://localhost:5000/api/';
-  }
+  static const String apiBaseUrl = 'http://localhost:5000/api/';
   // Storage Keys
   static const String tokenKey = 'CACHED_AUTH_TOKEN';
   static const String userKey = 'CACHED_USER_DATA';
