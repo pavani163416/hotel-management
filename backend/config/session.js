890474b8f3d76252f5540fe19ca0425e8dc52b62
p0154avani-creator <p0154avani@gmail.com>
2026-05-26 15:56:48 +0530

    Fix SyntaxError: change connect-redis import to default export

--- backend/config/session.js
+++ backend/config/session.js
@@ -1,5 +1,5 @@
 import session from "express-session";
-import { RedisStore } from "connect-redis";
+import RedisStore from "connect-redis";
 import { getRedisClient, isRedisReady } from "./redis.js";
 
 export const createSessionMiddleware = () => {
