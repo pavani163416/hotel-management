d831ac223839641391cfd9012a54d0208a796f95
p0154avani-creator <p0154avani@gmail.com>
2026-05-26 10:51:07 +0530

    fix: install sonner in admin app for public support toasts

--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -4,8 +4,11 @@ import App from "./App";
 import "./index.css";
 import { ErrorBoundary } from "./components/ErrorBoundary";
 
+import { Toaster } from "sonner";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <ErrorBoundary>
     <App />
+    <Toaster position="top-right" richColors />
   </ErrorBoundary>
 );
