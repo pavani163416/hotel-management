998c34d94821a73e13564afd5970e2197780592f
Abdulrahima2024 <Abdulrahima2024@users.noreply.github.com>
2026-06-16 15:35:49 +0530

    fix: import dio/dio.dart in auth_provider.dart

--- hotelmanag/lib/core/providers/auth_provider.dart
+++ hotelmanag/lib/core/providers/auth_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
+import 'package:dio/dio.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:google_sign_in/google_sign_in.dart';
