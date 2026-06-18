72dfb01b13caeb62ade9658d34a88593126b98b7
p0154avani-creator <p0154avani@gmail.com>
2026-06-18 14:00:17 +0530

    Fix ERR_MODULE_NOT_FOUND in serviceRoutes.js

--- backend/routes/serviceRoutes.js
+++ backend/routes/serviceRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { requestService } from "../controllers/serviceController.js";
-import { verifyCustomerToken } from "../middleware/customerAuth.js";
+import { protect } from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.post("/request", verifyCustomerToken, requestService);
+router.post("/request", protect, requestService);
 
 export default router;
